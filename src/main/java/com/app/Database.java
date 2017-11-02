e7e41374d150a9df1b6e53b0a00a8afc644ef725
Joshua Teague Van Pelt <joshuavanp@gmail.com>
2017-11-02 11:50:09 -0600

    cleanup

--- src/main/java/com/app/Database.java
+++ src/main/java/com/app/Database.java
@@ -1,7 +1,39 @@
+package com.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Database
 {
+
     public static void main( String[] args )
     {
+        new Database().run();
+    }
+
+    private Connection connection = null;
+
+    public Connection getConnection() {
+        if (connection == null) {
+            synchronized (this) {
+                if (connection == null) {
+                    try {
+                        connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+                    } catch (SQLException ex) {
+                        Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }
+        return connection;
+    }
+
+
+
+    public void run(){
 
     }
 }
