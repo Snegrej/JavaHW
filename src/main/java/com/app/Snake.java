e4ff1cf8d4fa8ecbaa4de90bb8d208531e6af706
Joshua Teague Van Pelt <joshuavanp@gmail.com>
2017-12-10 17:06:30 -0700

    first commit

---
+++ src/main/java/com/app/Snake.java
@@ -0,0 +1,19 @@
+public class Snake
+{
+    //Size of blocks (pixels)
+    private int blockDimensions = 15;
+
+    private int numRows = 16;
+    private int numColumns = 16;
+
+    //Game board
+    private int[] grid;
+    //Last dot of snake
+    private *int tail;
+
+
+    public static void main( String[] args )
+    {
+
+    }
+}
